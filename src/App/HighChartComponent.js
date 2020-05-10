6e3497e9569a97afb555b9ca27014b04af32a717
Medbrat31 <dmitrii.prasolov@gmail.com>
2020-05-10 11:47:01 +0300

    Highcharts added

--- src/App/HighChartComponent.js
+++ src/App/HighChartComponent.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
+import HighchartsReact from 'highcharts-react-official';
+import Highcharts from 'highcharts';
 
 const HighChartStyled = styled.div`
     
 `;
 
 function HighChartComponent({hourly, daily}){
-    return(<HighChartStyled>This is a HighChart component</HighChartStyled>)
+
+    let chartOptions = {
+        xAxis: {
+            categories: ['A', 'B', 'C']
+        },
+        series: {
+            data: [1, 2, 3]
+        }
+    };
+
+    return(<HighChartStyled>This is a HighChart component
+        <HighchartsReact highcharts={Highcharts} options={chartOptions} />
+    </HighChartStyled>)
 }
 
 export default HighChartComponent;
